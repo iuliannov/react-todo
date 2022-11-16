97e77618220ec1b5eefe40ebc58cfa7fb83bd9e2
iuliannov <contact@iuliannov.com>
2022-11-17 00:49:21 +0200

    finished adding and deleting logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { RefObject, useRef, useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
 
 type Todo = {
   text: string;
@@ -8,15 +8,23 @@ type Todo = {
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([{ text: "Initial Todo" }]);
+  const [inputValue, setInputValue] = useState<string>();
   const inputRef = useRef<HTMLInputElement>(null);
 
+  useEffect(() => inputRef.current?.focus());
+
   function addTodo() {
-    const newTodo = { text: inputRef.current?.value } as Todo;
+    inputRef.current?.focus();
+    const newTodo = { text: inputValue } as Todo;
     if (!Boolean(newTodo.text.trim())) {
       throw new Error("Text is empty");
     }
-
     setTodos((oldTodos) => [...oldTodos, newTodo]);
+    setInputValue("");
+  }
+
+  function deleteTodo(todo: Todo) {
+    setTodos((oldTodos) => [...oldTodos].filter((e) => e !== todo));
   }
 
   return (
@@ -24,16 +32,22 @@ function App() {
       <h1>Todos</h1>
       <div className="inputForm">
         <input
-          className="todoinput"
           ref={inputRef}
+          className="todoinput"
+          onChange={(e) => setInputValue(e.target.value)}
           type="text"
+          value={inputValue}
           placeholder="What woult you like to remember?"
         />
         <button onClick={addTodo}>Add todo</button>
       </div>
-      {todos.map((e) => (
-        <li>{e.text}</li>
-      ))}
+      <div>
+        {todos.map((e) => (
+          <li>
+            {e.text} <button onClick={() => deleteTodo(e)}>Delete</button>
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
