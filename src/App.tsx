b7279483afe28ffc0a2e8605e5e53f12f034f4ee
iuliannov <contact@iuliannov.com>
2022-11-17 00:21:41 +0200

    finish default logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
-import { useState } from "react";
+import "./App.css";
+
+import { RefObject, useRef, useState } from "react";
+
+type Todo = {
+  text: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState<Todo[]>([{ text: "Initial Todo" }]);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  return <div className="App"></div>;
+  function addTodo() {
+    const newTodo = { text: inputRef.current?.value } as Todo;
+    if (!Boolean(newTodo.text.trim())) {
+      throw new Error("Text is empty");
+    }
+
+    setTodos((oldTodos) => [...oldTodos, newTodo]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Todos</h1>
+      <div className="inputForm">
+        <input
+          className="todoinput"
+          ref={inputRef}
+          type="text"
+          placeholder="What woult you like to remember?"
+        />
+        <button onClick={addTodo}>Add todo</button>
+      </div>
+      {todos.map((e) => (
+        <li>{e.text}</li>
+      ))}
+    </div>
+  );
 }
 
 export default App;
